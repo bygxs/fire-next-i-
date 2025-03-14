a33067d94610706f3722e180e788906e9565aaa9
bygxs <biniyamyohannes@icloud.com>
2025-03-14 01:21:51 +0100

    now the app is pwa insallable

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const CACHE_NAME = "your-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  // Add other files you want to cache
+];
+
+// Install event
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
